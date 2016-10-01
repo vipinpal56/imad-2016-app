c416b1f6a3144e0f57145c0a3697ef6e3b6b31f4
vipinpal56 <vipinpal56@gmail.com>
2016-10-01 08:56:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-  res.send(path.join(__dirname, 'ui', 'article-one.html'));
-        
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two',function(req,res){
